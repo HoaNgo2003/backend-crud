2947ce1a768bf1b29b04b813f3ac2963f042ea6f
Hoa Ngo <hngo34048@gmail>
2024-07-26 21:19:56 +0700

    update

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,13 +1,13 @@
 import axois from "axios"
 export const jwt = localStorage.getItem("jwt")
-export const API_BASE_URL = "http://localhost:3000"
-export const api = axois.create({
-    baseURL: API_BASE_URL,
-    headers:{
-        "Authorization": `Bearer ${jwt}`,
-        'Content-Type':'application/json'
-    }
-})
+export const API_BASE_URL = "https://nest-backend-hkem.onrender.com"
+// export const api = axois.create({
+//     baseURL: API_BASE_URL,
+//     headers:{
+//         "Authorization": `Bearer ${jwt}`,
+//         'Content-Type':'application/json'
+//     }
+// })
 export const config = {
     headers: {
       'Authorization': 'Bearer ' + localStorage.getItem('access_token')
